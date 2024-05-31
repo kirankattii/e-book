f48a1ac1c31dfcb6b7210377029d3719ddabc681
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-05-31 13:13:24 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from 'express'
 
+import globalErrorHandler from './middlewares/globalErrorHandler'
+
 
 const app = express()
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res,next)=>{
   res.send({message:"Kirankatti"})
 })
+
+// global error handler
+
+app.use(globalErrorHandler)
+
+
 
 
 export default app
