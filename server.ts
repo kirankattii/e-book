b2abdbbdd25d6f1034f0d89045900d2768723bd7
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-05-31 11:25:51 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,2 +1,11 @@
-console.log("welcome");
+import app from "./src/app";
+
+const startServer = ()=>{
+  const port = process.env.PORT || 7000
   
+  app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+    
+  })
+}
+startServer()
