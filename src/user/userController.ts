c84b9e32056111ae84e59b974ad4c1b1918c6069
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-06-02 09:44:13 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt'
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 import { User } from "./userTypes";
+import { error } from "console";
 
 const createUser = async(req:Request,res:Response,next:NextFunction)=>{
 
@@ -24,7 +25,7 @@ const createUser = async(req:Request,res:Response,next:NextFunction)=>{
   try {
     const user = await userModel.findOne({email}) 
     if(user){
-      const error = createHttpError(400,"User already exist with this emai;")
+      const error = createHttpError(400,"User already exist with this email")
       return next(error)
     }
     
@@ -36,10 +37,10 @@ const createUser = async(req:Request,res:Response,next:NextFunction)=>{
 
   // password --> hash
 let newUser:User
+try {
   const hashedPassword = await bcrypt.hash(password,10)
-  try {
-      newUser =await  userModel.create({
-      name,email,hashedPassword
+  newUser =await  userModel.create({
+      name,email, password: hashedPassword 
     })
     
   } catch (error) {
@@ -58,3 +59,55 @@ let newUser:User
     return next(createHttpError(500,"Error While signin JWT TOken"))
   }
 }
+
+// login user
+const loginUser = async (req:Request,res:Response,next:NextFunction)=>{
+  const {email,password} = req.body
+
+  if(!email || !password){
+    return next(createHttpError(400,"All fields ar required"))
+  }
+  
+  let user:User |  null;
+  try { 
+      user = await userModel.findOne({email})
+    if(!user){
+      return next(createHttpError(404,"User Not Found"))
+    }
+  }
+ catch (error) {
+    return next(createHttpError(500,"Error while getting loginuser"))
+  }
+
+
+  // Compare passwords
+  try {
+    console.log("Comparing passwords:", { plainPassword: password, hashedPassword: user.password });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(createHttpError(400, "Username or Password incorrect"));
+    }
+
+    // Create access token
+    try {
+      const token = sign({ sub: user._id }, config.jwtSecret as string, {
+        expiresIn: "7d",
+        algorithm: "HS256"
+      });
+
+      res.json({ accessToken: token });
+    } catch (tokenError) {
+      console.error("JWT Token error:", tokenError);
+      return next(createHttpError(500, "Error while signing JWT token"));
+    }
+  } catch (compareError) {
+    console.error("Password comparison error:", compareError);
+    return next(createHttpError(500, "Error while comparing passwords"));
+  }
+
+
+
+}
+
+
+export {createUser,loginUser}
--- src/user/userTypes.ts
+++ src/user/userTypes.ts
@@ -1,6 +1,6 @@
 export interface User{
-  _id:String;
-  name:String;
-  email:String;
-  password:String;
+  _id:string;
+  name:string;
+  email:string;
+  password:string;
 }
