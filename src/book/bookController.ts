178fe5d45bd95bcbd395550c9be093675b22cc82
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-06-05 21:38:35 +0530

    add delete book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -187,6 +187,45 @@ const getSingleBook =  async (req: Request, res: Response, next: NextFunction) =
 
 }
 
+const deleteBook = async (req: Request, res: Response, next: NextFunction) =>{
+   const bookId = req.params.bookId
+
+   const book = await bookModel.findOne({_id:bookId})
+
+   if(!book){
+    return next(createHttpError(404,"Book not found"))
+   }
+
+    // Check access
+    const _req = req as AuthRequest;
+    if (book.author.toString() !== _req.userId) {
+      return next(createHttpError(403, "You can not update others book"));
+    }
+
+    const coverFileSplit = book.coverImage.split('/')
+    
+    const coverImagePublicId = coverFileSplit.at(-2) + "/" +(coverFileSplit.at(-1)?.split('.').at(-2))
+
+  
+
+    const bookFileSplits = book.file.split('/')
+
+    const bookFilePublicId = bookFileSplits.at(-2) + "/" +bookFileSplits.at(-1)
+
+      console.log("coverFileSplit",bookFilePublicId);
+
+     await cloudinary.uploader.destroy(coverImagePublicId)
+
+     await cloudinary.uploader.destroy(bookFilePublicId,{
+      resource_type:"raw"
+     })
+
+     await bookModel.deleteOne({_id:bookId});
+
+    return res.sendStatus(204)
+
+}
 
 
-export { createBook, updateBook,listBooks, getSingleBook};
+
+export { createBook, updateBook,listBooks, getSingleBook,deleteBook};
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createBook, getSingleBook, listBooks, updateBook } from './bookController'
+import { createBook, deleteBook, getSingleBook, listBooks, updateBook } from './bookController'
 import multer from 'multer'
 import path from 'node:path'
 import authenticate from '../middlewares/authenticate'
@@ -27,6 +27,9 @@ bookRouter.patch('/:bookId', authenticate, upload.fields([
 
 
 bookRouter.get('/',listBooks)
+
 bookRouter.get('/:bookId',getSingleBook)
+
+bookRouter.delete('/:bookId', authenticate,deleteBook)
 
 export default bookRouter
